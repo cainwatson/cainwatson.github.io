9600f211607ebc41402925a235a22cac3b283d75
Cain Watson <cainwatson00@gmail.com>
2020-06-24 08:49:03 -0500

    feat: create CaseStudyList component

---
+++ src/components/home/CaseStudyList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+import CaseStudy, { caseStudyPropType } from './CaseStudy'
+
+const Container = styled.section``
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`
+
+const CaseStudyList = ({ caseStudies }) => (
+  <Container>
+    <List>
+      {caseStudies.map(caseStudy => (
+        <li key={caseStudy.name}>
+          <CaseStudy caseStudy={caseStudy} />
+        </li>
+      ))}
+    </List>
+  </Container>
+)
+
+CaseStudyList.propTypes = {
+  caseStudies: PropTypes.arrayOf(caseStudyPropType).isRequired,
+}
+
+export default CaseStudyList
