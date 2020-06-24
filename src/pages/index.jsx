79bf3fd443eab2237c85a0d291fbaa354b6d36b0
Cain Watson <cainwatson00@gmail.com>
2020-06-24 08:49:28 -0500

    feat: add CaseStudyList to home page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 import SEO from '@components/SEO'
 import Layout from '@components/Layout'
 import HomeHero from '@components/home/HomeHero'
+import CaseStudyList from '@components/home/CaseStudyList'
+
+const Content = styled.div`
+  padding: 80px;
+`
 
 const HomePage = () => (
   <Layout>
     <SEO title="Home" />
     <HomeHero />
+    <Content>
+      <CaseStudyList
+        caseStudies={[
+          {
+            name: 'Draw It!',
+            description:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            linkSourceCode: 'https://github.com/JustDrawIt/drawit',
+            linkLive: 'https://cainwatson.com/drawit',
+            images: [
+              {
+                src: 'https://via.placeholder.com/600',
+                description: 'Lorem ipsum dolor sit amet.',
+              },
+            ],
+          },
+        ]}
+      />
+    </Content>
   </Layout>
 )
 
