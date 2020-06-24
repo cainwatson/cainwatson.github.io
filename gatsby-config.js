04aa5d306476e7ea84a351804dd4a9959a5ca0bb
Cain Watson <cainwatson00@gmail.com>
2020-06-24 01:35:58 -0500

    feat: add import alias plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,14 @@ module.exports = {
       },
     },
     `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-alias-imports`,
+      options: {
+        aliases: {
+          '@components': `src/components`,
+        },
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
