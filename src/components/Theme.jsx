8f4a7d4b47e62fe7b19fd7a5b0f2ca0b976cbda2
Cain Watson <cainwatson00@gmail.com>
2020-06-24 02:06:14 -0500

    feat: create Theme component

---
+++ src/components/Theme.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ThemeProvider } from 'emotion-theming'
+
+const theme = {
+  colors: {
+    primary: '#011627',
+    secondary: '#CC28A3',
+    blue: '#654AA3',
+    pink: '#C789D6',
+  },
+  fontFamily: {
+    header: '',
+    text: '',
+  },
+  fontSize: {},
+}
+
+const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+)
+
+Theme.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Theme
