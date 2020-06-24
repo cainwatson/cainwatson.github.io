906f7ab1e75c3986ad8b12fd51613da2250536b9
Cain Watson <cainwatson00@gmail.com>
2020-06-24 03:57:54 -0500

    feat: add background image to hero

--- src/components/home/HomeHero.jsx
+++ src/components/home/HomeHero.jsx
@@ -2,17 +2,32 @@ import React from 'react'
 import HomeCard from './HomeCard'
 import styled from '@emotion/styled'
 
+import BackgroundImage from '../../images/home-hero-background.svg'
+
+const Background = styled(BackgroundImage)`
+  position: absolute;
+  top: calc(-58vh + -20vw);
+  left: calc(-45vh + 10vw);
+  width: calc(233vh + 30vw);
+  height: auto;
+  z-index: -1;
+`
+
 const Container = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 20px;
 `
 
 const HomeHero = () => (
-  <Container>
-    <HomeCard />
-  </Container>
+  <>
+    <Background />
+    <Container>
+      <HomeCard />
+    </Container>
+  </>
 )
 
 HomeHero.propTypes = {}
