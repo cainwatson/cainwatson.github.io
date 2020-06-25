8a0502f480e083a656cee241eb2eaaf1bb9e2a03
Cain Watson <cainwatson00@gmail.com>
2020-06-25 00:50:58 -0500

    feat: update manifest fields

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `cainwatson.com`,
+        short_name: `cainwatson`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#FCEFF9`,
+        theme_color: `#011627`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-emotion`,
