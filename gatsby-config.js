48ee238d785945ad324b41942d53cdcb8c40e10d
Cain Watson <cainwatson00@gmail.com>
2020-07-05 13:36:28 -0500

    feat: enable gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,8 +103,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-preact`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
