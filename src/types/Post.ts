4d11120e94c442858739362ba9d81faa9b4d3188
Cain Watson <cainwatson00@gmail.com>
2020-07-13 20:32:24 -0500

    chore: add Post type

---
+++ src/types/Post.ts
@@ -0,0 +1,18 @@
+interface Post {
+  excerpt: string
+  frontmatter: {
+    slug: string
+    title: string
+    tags: string[]
+    date_published: string
+    date_updated?: string
+    date_published_formatted: string
+    date_updated_formatted?: string
+  }
+  fields: {
+    slug: string
+    readingTime: {
+      text: string
+    }
+  }
+}
